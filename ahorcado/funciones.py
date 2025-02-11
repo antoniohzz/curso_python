ea266a66cff009143d657a21ce6dffd61991eb67
antoniohzz <antoniibasz@gmail.com>
2025-02-11 14:51:18 -0700

    Obtiene e imprime palabras

--- ahorcado/funciones.py
+++ ahorcado/funciones.py
@@ -9,7 +9,42 @@ def carga_archivo_texto(archivo:str)->list:
         oraciones = f.readlines()
     return oraciones
 
+
+def carga_plantillas(nombre_plantilla:str) -> dict:
+    '''
+    Carga las plantillas del juego a partir de un archivode texto
+    '''
+    plantillas = {}
+    for i in range(6):
+        plantillas[i] = carga_archivo_texto(f'./ahorcado/plantillas/{nombre_plantilla}-{i}.txt')
+    return plantillas
+
+def despliega_plantilla(diccionario:dict, nivel:int):
+    '''
+    Despliega una plantilla del juego
+    '''
+
+    if nivel in diccionario:
+        template = diccionario[nivel]
+        for renglon in template:
+            print(renglon)
+
+def obten_palabras(lista:str)->list:
+    '''
+    Obtiene las palabras de un texto
+    '''
+    texto = ' '.join(lista[120:])
+    palabras = texto.split()
+    minusculas = [palabra.lower() for palabra in palabras]
+    set_palabras = set(minusculas)
+    return list(set_palabras)
+
 if __name__ == '__main__':
-    lista = carga_archivo_texto('./ahorcado/plantillas/plantilla-0.txt')
-    for elemento in lista:
-        print(elemento)
+    plantillas = carga_plantillas('plantilla')
+    despliega_plantilla(plantillas, 5)
+    lista_oraciones = carga_archivo_texto('./ahorcado/datos/pg15532.txt')
+    lista_palabras = obten_palabras(lista_oraciones)
+    print(lista_palabras[:50])
+
+
+    
