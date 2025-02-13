da62d8a54fedda399b72084d7aeab41b91306364
antoniohzz <antoniibasz@gmail.com>
2025-02-13 14:50:21 -0700

    funcion main() de app, con error al restar oportunidades

--- ahorcado/funciones.py
+++ ahorcado/funciones.py
@@ -60,7 +60,8 @@ def adivina_letra(abc:dict, palabra:str, letras_adivinadas:set, turnos:int):
         else:
             palabra_oculta += "_"
     print(f'Tienes {turnos} oportunidades de fallar')
-    print(f'El abecedario es: {abc}')
+    abcd = ' '.join(abc.values())
+    print(f'El abecedario es: {abcd}')
     print(f'La palabra es: {palabra_oculta}')
     letra = input('Ingresa una letra: ')
     letra = letra.lower()
