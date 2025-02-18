d4bf23ecb3555a0a902b3c236ddfc53797a48c5a
antoniohzz <antoniibasz@gmail.com>
2025-02-18 14:51:22 -0700

    Agregué clase Athlete

---
+++ games/Athlete.py
@@ -0,0 +1,23 @@
+class Athlete:
+    '''Athlete class, with only name attribute'''
+    def __init___ (self, name:str):
+        self.name = name
+    
+    def __str__(self):
+        return f"Athlete: {self.name}"
+    
+    def __repr__(self):
+        return f"Athlete:('{self.name}')"
+    
+    def display(self):
+        print(f"{self.name}")
+    
+if __name__ == "__main__":
+    a = Athlete("Ana G.")
+    a.display()
+    print(a)
+    print(repr(a))
+    print(f"a: {id(a)}")
+    b = eval(repr(a))
+    print(f"b: {id(b)}")
+    
