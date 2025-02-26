136c8c7d11b0b0a33283b196710ac0ba624af465
antoniohzz <antoniibasz@gmail.com>
2025-02-26 14:48:34 -0700

    Iniciamos torneo con 4 equipos

--- games/main.py
+++ games/main.py
@@ -5,6 +5,7 @@ from Team import Team
 from Game import Game
 import json
 
+
 def main(archivo_torneo:str):
     '''Funcion principal del juego'''
     if archivo_torneo != "":
@@ -13,13 +14,26 @@ def main(archivo_torneo:str):
     else:
         players_mexico = ['Chicharito', 'Piojo', 'Chucky', 'Tecatito', 'Gullit', 'Ochoa', 'Guardado', 'Herrera', 'Layun', 'Moreno', 'Araujo']
         players_españa = ['Casillas', 'Ramos', 'Pique', 'Alba', 'Iniesta', 'Silva', 'Isco', 'Busquets', 'Costa', 'Morata', 'Asensio']
+        players_brasil = ['Neymar', 'Coutinho', 'Marcelo', 'Casemiro', 'Alisson', 'Thiago', 'Firmino', 'Jesus', 'Silva', 'Paulinho', 'Danilo']
+        players_argentina = ['Messi', 'Aguero', 'Di Maria', 'Higuain', 'Mascherano', 'Otamendi', 'Rojo', 'Banega', 'Dybala', 'Perez', 'Mercado']
         lista_mexico = [Athlete(x) for x in players_mexico]
         lista_españa = [Athlete(x) for x in players_españa]
+        lista_brasil = [Athlete(x) for x in players_brasil]
+        lista_argentina = [Athlete(x) for x in players_argentina]
         soccer = Sport("Soccer", 11, "FIFA")
         mexico = Team("Mexico", soccer, lista_mexico)
         españa = Team("España", soccer, lista_españa)
-        juego = Game(mexico, españa)
-        torneo = [juego.to_json()]
+        brasil = Team("Brasil", soccer, lista_brasil)
+        argentina = Team("Argentina", soccer, lista_argentina)
+        equipos = [mexico, españa, brasil, argentina]
+        d = {}
+        for local in equipos:
+            for visitante in equipos:
+                if local != visitante:
+                    d[juego.to_json()['A']['name']] = juego.to_json()
+        torneo = list(d.values())
+        #juego = Game(mexico, españa)
+        #torneo = [juego.to_json()]
         archivo_torneo = "torneo.json"
         with open(archivo_torneo, "w", encoding='utf-8') as f:
             json.dump(torneo, f, ensure_ascii=False, indent=4)
