1257f129442205b4c06a72fef627831ab5f67d5c
antoniohzz <antoniibasz@gmail.com>
2025-03-03 14:26:49 -0700

    Finalizamos el programa games

--- games/game_logic.py
+++ games/game_logic.py
@@ -71,6 +71,51 @@ def json_to_tournament(torneo:dict)->list:
     '''Convierte un diccionario en una lista de juegos'''
     return [json_to_game(juego) for juego in torneo]
 
+def create_gamefile():
+    '''Crea un archivo de juego'''
+    players_mexico = ['Chicharito', 'Piojo', 'Chucky', 'Tecatito', 'Gullit', 'Ochoa', 'Guardado', 'Herrera', 'Layun', 'Moreno', 'Araujo']
+    players_españa = ['Casillas', 'Ramos', 'Pique', 'Alba', 'Iniesta', 'Silva', 'Isco', 'Busquets', 'Costa', 'Morata', 'Asensio']
+    players_brasil = ['Neymar', 'Coutinho', 'Marcelo', 'Casemiro', 'Alisson', 'Thiago', 'Firmino', 'Jesus', 'Silva', 'Paulinho', 'Danilo']
+    players_argentina = ['Messi', 'Aguero', 'Di Maria', 'Higuain', 'Mascherano', 'Otamendi', 'Rojo', 'Banega', 'Dybala', 'Perez', 'Mercado']
+    lista_mexico = [Athlete(x) for x in players_mexico]
+    lista_españa = [Athlete(x) for x in players_españa]
+    lista_brasil = [Athlete(x) for x in players_brasil]
+    lista_argentina = [Athlete(x) for x in players_argentina]
+    soccer = Sport("Soccer", 11, "FIFA")
+    mexico = Team("Mexico", soccer, lista_mexico)
+    españa = Team("España", soccer, lista_españa)
+    brasil = Team("Brasil", soccer, lista_brasil)
+    argentina = Team("Argentina", soccer, lista_argentina)
+    equipos = [mexico, españa, brasil, argentina]
+    d = {}
+    for local in equipos:
+        for visitante in equipos:
+            if local != visitante:
+                juego = Game(local, visitante)
+                partido = f'{local} - {visitante}'
+                partido_2 = f'{visitante} - {local}'
+                if partido not in d and partido_2 not in d:
+                    d[partido] = juego.to_json()
+    #print(d.keys())
+    torneo = list(d.values())
+    #juego = Game(mexico, españa)
+    #torneo = [juego.to_json()]
+    archivo_torneo = "torneo.json"
+    with open(archivo_torneo, "w", encoding='utf-8') as f:
+        json.dump(torneo, f, ensure_ascii=False, indent=4)
+    print(f"Se escribió el archivo {archivo_torneo} con un torneo de {len(torneo)} juego(s)")
+
+def play_game(torneo:dict):
+    '''Jugar todos los juegos del torneo'''
+    for juego in torneo:
+            A = Team(juego['A']['name'], Sport(juego['A']['sport']['name'], juego['A']['sport']['players'], juego['A']['sport']['league']), [Athlete(x['name']) for x in juego['A']['players']])
+            B = Team(juego['B']['name'], Sport(juego['B']['sport']['name'], juego['B']['sport']['players'], juego['B']['sport']['league']), [Athlete(x['name']) for x in juego['B']['players']])
+            game = Game(A, B)
+            game.play()
+            print(game)
+            juego['score'] = game.score
+            print("------------------------------------")
+
 if __name__ == '__main__':
     archivo = "torneo.json"
     with open(archivo, 'r') as f:
--- games/main.py
+++ games/main.py
@@ -11,49 +11,15 @@ def main(archivo_torneo:str):
     '''Funcion principal del juego'''
     if archivo_torneo != "":
         with open(archivo_torneo, "r") as file:
-            data = json.load(file)
+            torneo = json.load(f)
     else:
-        players_mexico = ['Chicharito', 'Piojo', 'Chucky', 'Tecatito', 'Gullit', 'Ochoa', 'Guardado', 'Herrera', 'Layun', 'Moreno', 'Araujo']
-        players_españa = ['Casillas', 'Ramos', 'Pique', 'Alba', 'Iniesta', 'Silva', 'Isco', 'Busquets', 'Costa', 'Morata', 'Asensio']
-        players_brasil = ['Neymar', 'Coutinho', 'Marcelo', 'Casemiro', 'Alisson', 'Thiago', 'Firmino', 'Jesus', 'Silva', 'Paulinho', 'Danilo']
-        players_argentina = ['Messi', 'Aguero', 'Di Maria', 'Higuain', 'Mascherano', 'Otamendi', 'Rojo', 'Banega', 'Dybala', 'Perez', 'Mercado']
-        lista_mexico = [Athlete(x) for x in players_mexico]
-        lista_españa = [Athlete(x) for x in players_españa]
-        lista_brasil = [Athlete(x) for x in players_brasil]
-        lista_argentina = [Athlete(x) for x in players_argentina]
-        soccer = Sport("Soccer", 11, "FIFA")
-        mexico = Team("Mexico", soccer, lista_mexico)
-        españa = Team("España", soccer, lista_españa)
-        brasil = Team("Brasil", soccer, lista_brasil)
-        argentina = Team("Argentina", soccer, lista_argentina)
-        equipos = [mexico, españa, brasil, argentina]
-        d = {}
-        for local in equipos:
-            for visitante in equipos:
-                if local != visitante:
-                    juego = Game(local, visitante)
-                    partido = f'{local} - {visitante}'
-                    partido_2 = f'{visitante} - {local}'
-                    if partido not in d and partido_2 not in d:
-                        d[partido] = juego.to_json()
-        #print(d.keys())
-        torneo = list(d.values())
-        #juego = Game(mexico, españa)
-        #torneo = [juego.to_json()]
+        gl.create_gamefile()
         archivo_torneo = "torneo.json"
-        with open(archivo_torneo, "w", encoding='utf-8') as f:
-            json.dump(torneo, f, ensure_ascii=False, indent=4)
-        print(f"Se escribió el archivo {archivo_torneo} con un torneo de {len(torneo)} juego(s)")
+        with open(archivo_torneo, "r") as f:
+            torneo = json.load(f)
+
     #Jugar todos los juegos del torneo
-    for juego in torneo:
-        A = Team(juego['A']['name'], Sport(juego['A']['sport']['name'], juego['A']['sport']['players'], juego['A']['sport']['league']), [Athlete(x['name']) for x in juego['A']['players']])
-        B = Team(juego['B']['name'], Sport(juego['B']['sport']['name'], juego['B']['sport']['players'], juego['B']['sport']['league']), [Athlete(x['name']) for x in juego['B']['players']])
-        game = Game(A, B)
-        game.play()
-        print(game)
-        juego['score'] = game.score
-        print("------------------------------------")
-    
+    gl.play_game(torneo)
     #Calcular el tablero de puntuacion
     for juego in torneo:
         print(juego['score'])
