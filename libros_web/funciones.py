adbcd092cc804ae9da6c169f8ce33c97479a35d1
antoniohzz <antoniibasz@gmail.com>
2025-03-05 14:52:08 -0700

    Creamos el proyecto libros_web

---
+++ libros_web/funciones.py
@@ -0,0 +1,26 @@
+'''Archivo con las funciones necesarias de la Aplicacion Liberos Web'''
+import csv
+
+def lee_archivo_csv(archivo:str)->list:
+    '''Lee un archivo CSV y lo comvierte en una lista de diccionarios'''
+    with open(archivo, "r", encoding='utf8') as f:
+        return [x for x in csv.DictReader(f)]
+    
+def crea_diccionario_titulos(lista:list)->dict:
+    '''Çrea un diccionario con los titulos como clave y el resto de los datos como valores'''
+    return {x['title']: x for x in lista}
+
+def busca_en_titulo(diccionario, palabra)->list:
+    '''Busca palabra en titulo de la lista de diccionarios'''
+    lista = []
+    for titulo, libro in diccionario.items():
+        if 'rebels' in titulo.lower():
+            lista.append(libro)
+    return lista
+
+if __name__ == '__main__':
+    archivo_csv = 'booklist2000.csv'
+    lista_libros = lee_archivo_csv(archivo_csv)
+    diccionario_libros = crea_diccionario_titulos(lista_libros)
+    resultado = busca_en_titulo(diccionario_libros, 'rebels')
+    print(resultado)
