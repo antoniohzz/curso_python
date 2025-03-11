9de8b92222b36a68767953b181d01013b78761bb
antoniohzz <antoniibasz@gmail.com>
2025-03-11 14:40:19 -0700

    Ya quedo buscar por letra

--- libros_web/app.py
+++ libros_web/app.py
@@ -1,24 +1,50 @@
-'''Programa principal de Libros Web'''
-from flask import Flask, render_template, request
+''' Programa principal de Libros Web '''
+from flask import Flask, render_template, request 
 import funciones as fn
 
 app = Flask(__name__)
 
 archivo_csv = 'booklist2000.csv'
 lista_libros = fn.lee_archivo_csv(archivo_csv)
-diccionario_titulos = fn.crea_diccionario(lista_libros, 'title')
-diccionario_autores = fn.crea_diccionario(lista_libros, 'author')
+diccionario_id = fn.crea_diccionario(lista_libros,'id')
+diccionario_titulos = fn.crea_diccionario(lista_libros,'title')
+diccionario_autores = fn.crea_diccionario(lista_libros,'author')
 
 @app.route('/')
 def inicio():
+    ''' Página de inicio de la aplicación '''
     return render_template('index.html')
 
-@app.route('/titulo', methods=['GET','POST'])
+@app.route('/titulo', methods =['GET','POST'])
 def busqueda_titulo():
-    '''Página de búsqueda por título'''
-    titulo = request.form['titulo']
-    resultado = fn.busca_en_diccionario(diccionario_titulos, titulo)
-    return render_template('resultado.html', lista_libros=resultado)
+    ''' Página de búsqueda por título '''
+    resultado = []
+    if request.method == 'POST':
+        titulo = request.form['titulo']
+        resultado = fn.busca_en_diccionario(diccionario_titulos, titulo)
+        print(titulo)
+        print(resultado)
+    return render_template('titulo.html', lista_libros=resultado)
 
+@app.route('/libro/<id>', methods =['GET'])
+def libro(id:str):
+    ''' Página de información de un libro '''
+    if id in diccionario_id:
+        libro = diccionario_id[id]
+        return render_template('libro.html', libro=libro)
+    else:
+        return render_template('libro.html', libro=None)
+    
+@app.route('/letra/', methods =['GET'] )
+def plantilla_letra():
+    '''Pagina de busqueda por letra'''
+    return render_template('letra.html', lista_libros=[])
+
+@app.route('/letra/<letra>', methods =['GET'] )
+def busqueda_letra(letra:str):
+    ''' Página de búsqueda por letra '''
+    resultado = fn.libros_empiezan_con(lista_libros, letra)
+    return render_template('letra.html', lista_libros=resultado)
+    
 if __name__ == '__main__':
     app.run(debug=True)
--- libros_web/funciones.py
+++ libros_web/funciones.py
@@ -1,17 +1,17 @@
-'''Archivo con las funciones necesarias de la Aplicacion Liberos Web'''
+''' Archivo con las funciones necesarias de la Aplicación Libro Web'''
 import csv
 
 def lee_archivo_csv(archivo:str)->list:
-    '''Lee un archivo CSV y lo comvierte en una lista de diccionarios'''
+    ''' Lee un archivo CSV y lo convierte en una lista de diccionarios '''
     with open(archivo, "r", encoding='utf8') as f:
         return [x for x in csv.DictReader(f)]
-    
+
 def crea_diccionario(lista:list, llave:str)->dict:
-    '''Çrea un diccionario con los titulos como clave y el resto de los datos como valores'''
+    ''' Crea un diccionario con la palabra "llave" como clave y el resto de los datos como valores '''
     return {x[llave].lower(): x for x in lista}
 
 def busca_en_diccionario(diccionario:dict, palabra:str)->list:
-    '''Busca palabra en titulo de la lista de diccionarios'''
+    ''' Busca palabra en llave de la lista de diccionarios '''
     lista = []
     palabra = palabra.lower()
     for llave, libro in diccionario.items():
@@ -20,18 +20,16 @@ def busca_en_diccionario(diccionario:dict, palabra:str)->list:
     return lista
 
 def libros_empiezan_con(lista:list, letra:str)->list:
-    '''Busca libros que empiecen con letra'''
-
+    ''' Busca libros que empiecen con letra '''
     return [x for x in lista if x['title'].lower().startswith(letra.lower())]
-
 
 if __name__ == '__main__':
     archivo_csv = 'booklist2000.csv'
     lista_libros = lee_archivo_csv(archivo_csv)
-    diccionario_libros = crea_diccionario(lista_libros, 'title')
+    diccionario_libros = crea_diccionario(lista_libros,'title')
     resultado = busca_en_diccionario(diccionario_libros, 'rebels')
     print(resultado)
-    diccionario_autores = crea_diccionario(lista_libros, 'author')
+    diccionario_autores = crea_diccionario(lista_libros,'author')
     resultado = busca_en_diccionario(diccionario_autores, 'Sandra')
     print(resultado)
     resultado = libros_empiezan_con(lista_libros, 'a')
